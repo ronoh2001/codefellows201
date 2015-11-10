11cad408d1837385e4d1d37f7e0bc42eaf15cab9
Ron Oh <ronoh2001@gmail.com>
2015-11-09 19:17:53 -0800

    update readme file

--- guess.js
+++ guess.js
@@ -8,28 +8,39 @@
   
   var numberCorrect = 0;
   
-    var input1 = prompt("Does Ron have children?").toLowerCase();
+     function children() {
+        document.getElementById("child").src = "truck.jpg";
+    }
+     function hotel() {
+        document.getElementById("job").src = "hotel.png";
+    }
 
+    function cello() {
+        document.getElementById("cello").src = "cello.jpg";
+    }
+    function see() {
+        document.getElementById("glasses").src = "glass.jpg";
+    }
+    function live() {
+        document.getElementById("Seattle").src = "space.jpg";
+    }
+    function page() {
+        document.getElementById("old").src = "walker.jpg";
+    }
+    children();
+    var input1 = prompt("Does Ron have children?").toLowerCase();
+    
     if ((input1 === "yes")||(input1 === "y")) {
 
         numberCorrect++;  
         alert("You are right! He has two boys!" ) ;
-        children(); 
 
     }
      else  {
 
         alert("Sorry you're wrong.  He has 2 boys");
-        children();
-      
+        
     }
-    function children() {
-    var x = document.createElement("child");
-    x.setAttribute("src", "truck.jpg");
-    document.body.appendChild(x);
-}
-    // function children() {
-    // document.getElementById("img id=child1").src = "truck.jpg";
 //       function children() {
 //       var x = document.getElementById("child").src = "truck.jpg";
 //       document.getElementById("truck").innerHTML = x;
@@ -41,11 +52,11 @@
     if ((input2 === "yes")||(input2 === "y")) {
       numberCorrect++;
       alert("you're right! He is the General Manager at the Holiday Inn Express!");
-
+      hotel();
   } 
     else{
       alert("Sorry ...you're wrong.  He is the General Manager at the Holiday Inn Express");
-
+      hotel();
   }
 
 // function hotel() {
@@ -58,25 +69,27 @@
     if ((input3 === "yes")||(input3 === "y")) {
       numberCorrect++;
       alert("Your right he plays the cello!");
-      
+      cello();
     }
     else {
       alert("Sorry you are wrong.  He plays the cello");
-      
+      cello();
     }
-function cello() {
-    var x = document.getElementById("cello").src;
-    document.getElementById("instrument").innerHTML = x;
-}
+// function cello() {
+//     var x = document.getElementById("cello").src;
+//     document.getElementById("instrument").innerHTML = x;
+// }
 
     var input4=prompt("Does Ron  wear glasses?").toLowerCase();
 
     if ((input4 === "yes")||(input4 === "y")) {
       numberCorrect++;
       alert("Your right he wears glasses");
+      see();
     }
     else {
       alert("Sorry you are wrong.  He is blind as a bat");
+    see();
     }
 
 
@@ -85,9 +98,11 @@ function cello() {
     if (input5 == 9) {
       numberCorrect++;
       alert("Your right he has lived in Seattle for 9 years");
+      live();
     }
     else {
       alert("Sorry you are wrong, he has lived in Seattle for 9 years");
+      live();
     }
 
 
@@ -97,16 +112,18 @@ function cello() {
     numberCorrect++;
 
       alert("Your right! He is 42 years OLD!");
+      page();
     }
    else {
      alert("Sorry you are wrong");
+     page();
 
    }
      if (age <= 42) {
     alert('he is ' + differentAge +' years older than you');}
 
       else {
-       alert("wow he is" + " " + youngAge + " " + "younger than you are!");
+       alert("wow he is " + youngAge + " younger than you are!");
       }
 
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+This is my Readme file!
+
+Sorry, I had to delete and redo the repository, so I don't have all my updates :(
+
+This code was pair programmed with Yunjoo Kim.  She drove while I navigated.
+
+
+there were two skunks.  one is named 'In' the other is named 'Out'.  In was always outside and Out was always inside. One day Out's mom said to Out,"Out go outside and find In.  I've been looking for In all day long".  Out went outside and immediately came back with In.  Out's mom asked Out, 'Out how did you find In so fast?'.  Out replied, 'In stinks!'
+
